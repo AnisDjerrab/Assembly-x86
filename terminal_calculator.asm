03828b85abd5ae741d8f7f12077da2e20a92ba8e
AnisDjerrab <anisdjerrab@gmail.com>
2025-11-07 11:06:26 +0000

    Implemented Shift Function

--- terminal_calculator.asm
+++ terminal_calculator.asm
@@ -1,16 +1,16 @@
 segment .data
     CalculatorText db "                                ", 10, " sqrt()   DEL     OFF      AC   ", 10, "   7      8       9        /    ", 10, "   4      5       6        *    ", 10, "   1      2       3        +    ", 10, "   ^      0       =        +    ", 10, 10, "Press [h] to see your history", 10, "Press [SUPPR] for DEL", 10, "Press [x] for OFF", 10, "Press [r] for AC", 10, "Press [s] for sqrt()", 10
-    ; that's executed in compilation time !
-    shiftTable:
-        %assign i, 1
-        %rep 307
-            db 27, '[', '0'+i, 'C'
-            %assign i i+1
-        %endrep
     numberOfCaractersInNumberOne db 0
     numberOfCaractersInNumberTwo db 0
     BoolState db 1
+    ShiftAdress db 307
+    ShiftLeft db 27, '[', 'D'
+    ShiftRight db 27, '[', 'C'
 segment .bss
+    results resb 8
+    temp1 resb 32
+    temp2 resb 32
+    Shift resb 6
     AdressWhereToWrite resb 4
     number1 resb 9
     number2 resb 9
@@ -77,11 +77,15 @@ case2:
     jmp PrintNumber
 NonNumericCaraceter:
 PrintNumber:
-    ; print it
+    ; set the cursor at the right position
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, [shiftTable + AdressWhereToWrite]
+    ; print the number
     mov eax, 4
     mov ebx, 1
     mov ecx, buffer
-    mov edx, AdressWhereToWrite
+    mov edx, 1
     int 0x80
     jmp MainLoop
 
@@ -90,3 +94,95 @@ Exit:
     mov eax, 1
     xor ebx, ebx
     int 0x80
+
+
+
+
+ShiftCaracterToAnAdress:
+    push ebp
+    mov ebp, esp
+
+    mov ebp, [ebp + 8] ; this contains the length of the shift ! 
+
+    mov eax, [ShiftAdress]
+    sub eax, esi ; extracts the length of the first shift
+
+    push eax
+    call _convert_in_ASCII
+
+    mov esi, eax
+    mov [Shift], [ShiftLeft]
+    mov [Shift + 3], [esi]
+
+    ; reset the cursor postion to zero
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, Shift
+    mov edx, [esi + 4]
+    int 0x80
+
+    ; shift to the correct position
+    mov eax, ebp
+
+    push eax
+    call _convert_in_ASCII
+
+    mov esi, eax
+    mov [Shift], [ShiftRight]
+    mov [Shift + 3], [esi]
+
+    ; print the shift
+    mov eax, 4
+    mov ebx, 1
+    mov ecx, Shift
+    mov edx, [esi + 4]
+    int 0x80
+        
+    pop ebp
+    ret
+    
+
+
+
+_convert_in_ASCII:
+    push ebp
+    mov ebp, esp
+
+    mov eax, [ebp + 8]
+
+    mov esi, temp1
+    mov edx, 0
+    ; this produces first an inversed number that'll have to be reversed.
+loop3: 
+    mov ebx, 10
+    xor edx, edx
+    div ebx
+    add dl, '0'
+    mov [esi], dl
+    inc esi
+    cmp eax, 0
+    je code1
+    jmp loop3
+code1:    
+    mov eax, esi
+    sub eax, temp1
+    mov [index], eax
+    dec esi
+    mov edi, esi
+    mov esi, temp2
+loop4:
+    mov al, [edi]
+    mov [esi], al
+    cmp edi, temp1
+    je code2
+    dec edi
+    inc esi
+    jmp loop4 
+code2:
+    mov [results], temp2
+    mov [results + 4], [index]
+    mov eax, results
+    pop ebp
+    ret 
+
+
